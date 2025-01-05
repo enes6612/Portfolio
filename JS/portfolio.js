aa96aee0f56d7c8482eb2fef3d4deaa339c6ca01
Enes Tanriverdi <Enes.Tanriverdi@student.pxl.be>
2025-01-05 18:36:10 +0100

    portfolio-02

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -4,3 +4,18 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+const scrollToTopButton = document.getElementById('scrollToTop');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+scrollToTopButton.addEventListener('click', () => {
+    // Scroll langzaam terug naar boven
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
